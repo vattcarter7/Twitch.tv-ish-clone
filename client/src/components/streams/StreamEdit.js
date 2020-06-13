4982d4cb75c6e9c51461756f3179930a9eaf1e3d
vattcarter7 <vattcarter@gmail.com>
2020-06-13 13:15:22 +0700

    implemented lodash to pick particular key

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,17 +1,31 @@
 import React from 'react';
+import _ from 'lodash';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  onSubmit = (formValues) => {
+    console.log(formValues);
+  };
+
   render() {
     if (!this.props.stream) {
       return <div>Loading ...</div>;
     }
-    return <div>StreamEdit</div>;
+    return (
+      <div>
+        <h3>Edit a stream</h3>
+        <StreamForm
+          initialValues={_.pick(this.props.stream, 'title', 'description')}
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
   }
 }
 
@@ -19,4 +33,6 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
